d0a7feb76eaa489b33d85b355abf3ed121e58610
ricky <chenricky@gmail.com>
2022-03-13 09:32:30 -0700

    add color and labels

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 from turtle import Turtle, Screen
+import random
 tim = Turtle()
 screen = Screen()
+colours = ["red", "blue", "green", "yellow"]
+tim.color("black")
 tim.shape("circle")
 
+
 def move_forward():
-    tim.pencolor("black")
+    # tim.pencolor("black")
+    tim.color(random.choice(colours))
     tim.forward(10)
     tim.write("F")
 
 def move_backward():
-    tim.pencolor("black")
+    # tim.pencolor("black")
+    tim.color(random.choice(colours))
     tim.backward(10)
     tim.write("B")
 
 def move_counter_clockwise():
-    tim.pencolor("black")
+    # tim.pencolor("black")
+    tim.color(random.choice(colours))
     tim.right(10)
     tim.forward(10)
     tim.write("R")
 
 def move_clockwise():
-    tim.pencolor("black")
+    # tim.pencolor("black")
+    tim.color(random.choice(colours))
     tim.left(10)
     tim.forward(10)
     tim.write("L")
